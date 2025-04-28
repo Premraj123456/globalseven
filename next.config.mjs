83b151252616968b52bd2cb2d75dcf01e87fe7a0
Battle G <Premraj123456>
2025-04-28 22:22:27 +0530

    Image Tele Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images : {
-        domains: ['i.ibb.co', 'ibb.co'],
+        domains: ['i.ibb.co', 'ibb.co','telegram.org','api.telegram.org'],
     }
 };
 
