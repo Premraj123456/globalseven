f5264fe6ccc299c749138ce3115ee7824803a2a6
Battle G <Premraj123456>
2025-04-30 07:44:44 +0530

    google analytics addition

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G5Q431YDBN');
+          `}
+        </Script>
+      </head>
       <body
         className={`antialiased`}
       >
