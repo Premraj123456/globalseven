744d8116e8a1846fe66fbd819049bcb5cc10cce3
Battle G <Premraj123456>
2025-05-03 07:13:33 +0530

    google analytic edition

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,14 @@
-// import { Geist, Geist_Mono } from "next/font/google";
 import { Permanent_Marker } from 'next/font/google';
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Script from "next/script";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
 export const permanentMarker = Permanent_Marker({
   subsets: ['latin'],
-  weight: '400', // Permanent Marker only supports 400
-  variable: '--font-permanent-marker', // Optional: for Tailwind or custom CSS
+  weight: '400',
+  variable: '--font-permanent-marker',
 });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "GlobalSeven | Timely Insights: Breaking News Updates, Global Headlines, and Detailed Coverage",
@@ -29,20 +18,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
       <head>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-G5Q431YDBN"></Script>
-        <Script>
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-G5Q431YDBN');`}
-        </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G5Q431YDBN"
+          strategy="beforeInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G5Q431YDBN');
+            `,
+          }}
+        />
       </head>
-      <body
-        className={`antialiased `}
-      >
+      <body className="antialiased">
         <Header />
         {children}
         <Footer />
