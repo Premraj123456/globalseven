56a34cb4fadd437c9c05c8b154aded8cc811fb4d
Battle G <Premraj123456>
2025-05-03 07:35:27 +0530

    google analytic edition

--- app/components/GTag.js
+++ app/components/GTag.js
@@ -2,23 +2,23 @@
 
 import { useEffect } from 'react';
 
-export default function GTag() {
+export default function GoogleAnalytics() {
   useEffect(() => {
-    // Load the external script manually
-    const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-G5Q431YDBN';
-    script.async = true;
-    document.head.appendChild(script);
+    // Load the gtag.js script
+    const script1 = document.createElement('script');
+    script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-G5Q431YDBN';
+    script1.async = true;
+    document.head.appendChild(script1);
 
-    // Add inline script
-    const inlineScript = document.createElement('script');
-    inlineScript.innerHTML = `
+    // Inline configuration
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-G5Q431YDBN');
     `;
-    document.head.appendChild(inlineScript);
+    document.head.appendChild(script2);
   }, []);
 
   return null;
