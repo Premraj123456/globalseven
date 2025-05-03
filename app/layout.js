ab586c1427a72b107e6dd140b5c72d1d2bc8721b
Battle G <Premraj123456>
2025-05-03 08:17:19 +0530

    everything works from website to analytics. Caution: Everytime check in development mode when you edit something

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,45 @@
 // import { Geist, Geist_Mono } from "next/font/google";
-// import { Permanent_Marker } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Script from "next/script";
 
-
-// export const permanentMarker = Permanent_Marker({
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
 //   subsets: ["latin"],
-//   weight: "400", // Permanent Marker only supports 400
-//   variable: "--font-permanent-marker", // Optional: for Tailwind or custom CSS
 // });
 
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "GlobalSeven | Timely Insights: Breaking News Updates, Global Headlines, and Detailed Coverage",
   description: "Stay informed on global news quickly and easily with our well-structured blog posts.",
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G5Q431YDBN"
+          strategy="beforeInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G5Q431YDBN');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`antialiased`}
       >
