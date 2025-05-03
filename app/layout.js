c41a08f52b5acb4f3876002b9c22c51145b22002
Battle G <Premraj123456>
2025-05-03 07:57:38 +0530

    google analytic edition

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,30 @@
-import { Permanent_Marker } from 'next/font/google';
+// import { Geist, Geist_Mono } from "next/font/google";
+// import { Permanent_Marker } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Script from "next/script";
-import GTag from './components/GTag';
 
-export const permanentMarker = Permanent_Marker({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-permanent-marker',
-});
+
+// export const permanentMarker = Permanent_Marker({
+//   subsets: ["latin"],
+//   weight: "400", // Permanent Marker only supports 400
+//   variable: "--font-permanent-marker", // Optional: for Tailwind or custom CSS
+// });
+
 
 export const metadata = {
   title: "GlobalSeven | Timely Insights: Breaking News Updates, Global Headlines, and Detailed Coverage",
   description: "Stay informed on global news quickly and easily with our well-structured blog posts.",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        {/* ✅ Google Analytics Script - Load in <head> via strategy */}
-        <GTag /> {/* 👈 Load GTAG manually */}
+      <body
+        className={`antialiased`}
+      >
         <Header />
         {children}
         <Footer />
